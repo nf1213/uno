9c39b67e322aeb63aa4e5c0f9f1b32519aadd387
Nicole <nfelch121394@gmail.com>
2015-02-16 16:19:23 -0500

    Controller actions and index view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,21 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def index
+    @games = Game.all
+    @game = Game.new
   end
 
   def show
+    @game = Game.find(params[:id])
+  end
+
+  def create
+    @game = Game.new(player1: current_user)
+    if @game.save
+      redirect_to @game, notice: "New Game Started"
+    else
+      render :index
+    end
   end
 end
