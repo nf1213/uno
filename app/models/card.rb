969c4d5d63bb45f3b9d7cbb7f12323deef63add6
Nicole <nfelch121394@gmail.com>
2015-02-15 23:12:47 -0500

    Associations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,4 @@
 class Card < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :game
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,6 @@
 class Game < ActiveRecord::Base
+  belongs_to :player1, class_name: "User"
+  belongs_to :player2, class_name: "User"
+  belongs_to :player3, class_name: "User"
+  belongs_to :player4, class_name: "User"
 end
